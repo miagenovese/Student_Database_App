f8f1b60f251da7b179a0a239906abe802e690cc8
Mia <miagenovese@gmail.com>
2018-05-19 17:48:59 +0200

    Initialize constructor

--- Java Projects/src/studentdatabaseapp/Student.java
+++ Java Projects/src/studentdatabaseapp/Student.java
@@ -1,5 +1,37 @@
 package studentdatabaseapp;
+import java.util.Scanner;
 
 public class Student {
+	private String firstName;
+	private String lastName;
+	private int gradeYear;
+	private int studentID;
+	private String courses;
+	private int tuitionBalance;
+	private int costOfCourse = 600;
+	
+	// Constructor prompts user to enter student name and year
+	public Student() {
+		Scanner in = new Scanner(System.in);
+		System.out.print("Enter student first name: ");
+		this.firstName = in.nextLine();
+		
+		System.out.print("Enter student last name: ");
+		this.lastName = in.nextLine();
+		
+		System.out.print("1 - Freshman\n2 - Sophmore\n3 - Junior\n4 - Senior\nEnter student grade level: ");
+		this.gradeYear = in.nextInt();
+		System.out.println(firstName + " " + lastName + " " + gradeYear);
+	}
+	
+	// Generate and ID
+	
+	// Enroll in courses
+	
+	// View balance
+	
+	// Pay tuition
+	
+	// Show status
 
 }
--- Java Projects/src/studentdatabaseapp/StudentDatabaseApp.java
+++ Java Projects/src/studentdatabaseapp/StudentDatabaseApp.java
@@ -3,6 +3,10 @@ package studentdatabaseapp;
 public class StudentDatabaseApp {
 
 	public static void main(String[] args) {
+		Student stu1 = new Student();
+		// Ask how many users we want to add to database
+		
+		// Create n number of new students
 
 	}
 
