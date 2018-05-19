c5d08eea0c8b14f196d3b18bb36c38dcf4b3b300
Mia <miagenovese@gmail.com>
2018-05-19 19:19:06 +0200

    Implement pay tuition method

--- Java Projects/src/studentdatabaseapp/Student.java
+++ Java Projects/src/studentdatabaseapp/Student.java
@@ -6,7 +6,7 @@ public class Student {
 	private String lastName;
 	private int gradeYear;
 	private String studentID;
-	private String courses = null;
+	private String courses = "";
 	private int tuitionBalance = 0;
 	// Static variables (belongs to class and not instance of object)
 	private static int costOfCourse = 600;
@@ -50,18 +50,28 @@ public class Student {
 				tuitionBalance = tuitionBalance + costOfCourse;
 			}
 			else { 
-				System.out.println("Break!");
 				break; 
 			}
 		} while (1 != 0);
 		
 		System.out.println("ENROLLED IN: " + courses);
-		System.out.println("TUITION BALANCE: " + tuitionBalance);		
 	}
 	
 	// View balance
+	public void viewBalance() {
+		System.out.println("Your balance is: $" + tuitionBalance);
+	}
 	
 	// Pay tuition
+	public void payTuition() {
+		viewBalance();
+		System.out.print("Enter your payment: $");
+		Scanner in = new Scanner(System.in);
+		int payment = in.nextInt();
+		tuitionBalance = tuitionBalance - payment;
+		System.out.println("Thank you for your payment of $" + payment);
+		viewBalance();
+	}
 	
 	// Show status
 
